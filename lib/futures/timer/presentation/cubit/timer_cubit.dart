e6ef08f31ffcbbc70e404cad6788ad37596254bc
elsaramji <mahmoudelsaramegy@gmail.com>
2025-06-11 00:30:51 +0300

    - fix slow chanage value bug in timer ✅ - unit and intigration tast => passed ✅

--- lib/futures/timer/presentation/cubit/timer_cubit.dart
+++ lib/futures/timer/presentation/cubit/timer_cubit.dart
@@ -4,25 +4,29 @@ import 'package:meta/meta.dart';
 import 'package:pomotasks/config/themes/assets/constant_audio_path.dart';
 import 'package:pomotasks/core/value/constant_value.dart';
 import 'package:time/time.dart';
-
 part 'timer_state.dart';
 
 class TimerCubit extends Cubit<TimerState> {
   TimerCubit() : super(TimerInitial());
+  // timer state
   bool isPaused = false;
+  // audio player
   AudioPlayer audioPlayer = AudioPlayer();
+  // start timer
   startTimer({required int minutes, required int seconds}) async {
     isPaused = false;
     while (minutes >= 0 && isPaused == false) {
+      // seconds--;
       emit(TimerInitial());
-      await 1.seconds.delay;
       emit(TimerChanged(minutes: minutes, seconds: seconds--));
-
+      await 1.seconds.delay;
+      // minutes--;
       if (seconds == 0 && minutes > 0) {
         minutes--;
         seconds = ConstantValue.defaultSeconds;
         emit(TimerChanged(minutes: minutes, seconds: seconds));
       }
+      // finish and  play music
       if (minutes <= 0 && seconds < 0) {
         emit(TimerChanged(minutes: 0, seconds: 0));
         await audioPlayer.play(AssetSource(ConstantAudioPath.endSessionMusic));
@@ -31,13 +35,14 @@ class TimerCubit extends Cubit<TimerState> {
     }
   }
 
+  // pause timer
   pauseTimer() {
     isPaused = !isPaused;
   }
 
+  // reset timer
   resetTimer() async {
     isPaused = true;
-    await 1.seconds.delay;
     emit(
       TimerReset(
         seconds: ConstantValue.defaultSeconds,
