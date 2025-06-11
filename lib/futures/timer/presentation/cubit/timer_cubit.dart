3540887b65dbe689dc321174b7ac8f03c993d16f
elsaramji <mahmoudelsaramegy@gmail.com>
2025-06-11 03:19:25 +0300

    - make start button more stable and add state working ✅ -  over clicking bug ✅

--- lib/futures/timer/presentation/cubit/timer_cubit.dart
+++ lib/futures/timer/presentation/cubit/timer_cubit.dart
@@ -1,25 +1,37 @@
+import 'dart:developer';
+
 import 'package:audioplayers/audioplayers.dart';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:pomotasks/config/themes/assets/constant_audio_path.dart';
 import 'package:pomotasks/core/value/constant_value.dart';
 import 'package:time/time.dart';
+
 part 'timer_state.dart';
 
 class TimerCubit extends Cubit<TimerState> {
   TimerCubit() : super(TimerInitial());
   // timer state
   bool isPaused = false;
+  bool isWorking = false;
   // audio player
   AudioPlayer audioPlayer = AudioPlayer();
   // start timer
   startTimer({required int minutes, required int seconds}) async {
     isPaused = false;
+    if (!isWorking) {
+      await downTimer(minutes, seconds);
+    }
+  }
+
+  Future<void> downTimer(int minutes, int seconds) async {
     while (minutes >= 0 && isPaused == false) {
       // seconds--;
       emit(TimerInitial());
       emit(TimerChanged(minutes: minutes, seconds: seconds--));
+      isWorking = true;
       await 1.seconds.delay;
+    
       // minutes--;
       if (seconds == 0 && minutes > 0) {
         minutes--;
@@ -29,7 +41,9 @@ class TimerCubit extends Cubit<TimerState> {
       // finish and  play music
       if (minutes <= 0 && seconds < 0) {
         emit(TimerChanged(minutes: 0, seconds: 0));
-        await audioPlayer.play(AssetSource(ConstantAudioPath.endSessionMusic));
+        await audioPlayer.play(
+          AssetSource(ConstantAudioPath.endSessionMusic),
+        );
         break;
       }
     }
@@ -37,12 +51,14 @@ class TimerCubit extends Cubit<TimerState> {
 
   // pause timer
   pauseTimer() {
-    isPaused = !isPaused;
+    isPaused = true;
+    isWorking = false;
   }
 
   // reset timer
   resetTimer() async {
     isPaused = true;
+    isWorking = false;
     emit(
       TimerReset(
         seconds: ConstantValue.defaultSeconds,
