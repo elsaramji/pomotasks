74e74320118ae31e22515f06601fe5d87dc04c39
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-06-24 02:43:46 +0300

    add new color in disgen

--- lib/config/themes/colors/app_colors.dart
+++ lib/config/themes/colors/app_colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color primaryColor = Color(0xFFEB2933);
   static const Color primaryColor2 = Color(0xFfEDC9CC);
+  static const Color primaryColor3 = Color(0xff994D52);
   static const Color primaryColorBackground2 = Color(0xFfFAFAFA);
   static const Color primaryColorBackground = Color(0xFFFCF7F7);
   static const Color textPrimaryColor = Color(0xFF171212);
