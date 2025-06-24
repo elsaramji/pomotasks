7b41e7db1f4ae27b4c4eb7d80270e3a0a4ad8213
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-06-24 04:04:03 +0300

    - impl add task button.

--- lib/futures/tasks/presentation/functions/add_task.dart
+++ lib/futures/tasks/presentation/functions/add_task.dart
@@ -1,2 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:pomotasks/futures/tasks/presentation/widgets/add_task_sheet_body.dart';
 
-addTask(){}
+
+addTask(BuildContext context) {
+  showModalBottomSheet(
+    context: context,
+    builder: (context) => AddTaskSheetBody(),
+  );
+}
