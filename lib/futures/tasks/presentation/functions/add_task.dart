94b3ba7dc4d1ec6a5314dbf5b8b0eafbcae22df0
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-06-28 04:10:22 +0300

    - update show bottom sheet to apply new functions.

--- lib/futures/tasks/presentation/functions/add_task.dart
+++ lib/futures/tasks/presentation/functions/add_task.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:pomotasks/futures/tasks/presentation/widgets/add_task_sheet_body.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pomotasks/core/di/get_it_di.dart';
+import 'package:pomotasks/futures/tasks/presentation/cubit/tasks_data_cubit.dart';
+import 'package:pomotasks/futures/tasks/presentation/widgets/add_task_fom.dart';
 
 addTask(BuildContext context) {
-  showModalBottomSheet(
+  showBottomSheet(
     context: context,
-    builder: (context) => AddTaskSheetBody(),
+    builder: (context) {
+      return BlocProvider.value(
+       value: getIt.get<TasksDataCubit>(),
+        child: Builder(
+          builder: (context) {
+            return AddTaskForm();
+          },
+        ),
+      );
+    },
   );
 }
