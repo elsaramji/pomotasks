36210b281d86030449c6d24c780fb8fc184bc78b
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-06-28 04:13:30 +0300

    - update tasks mainview to apply observe state and buliding widget with ui  .✅

--- lib/futures/tasks/presentation/view/tasks_main_view.dart
+++ lib/futures/tasks/presentation/view/tasks_main_view.dart
@@ -1,22 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pomotasks/futures/tasks/presentation/cubit/tasks_data_cubit.dart';
 import 'package:pomotasks/futures/tasks/presentation/widgets/task_card.dart';
 import 'package:pomotasks/futures/tasks/presentation/widgets/tasks_app_bar.dart';
 
-class TasksMainView extends StatelessWidget {
+class TasksMainView extends StatefulWidget {
   const TasksMainView({super.key});
 
   @override
+  State<TasksMainView> createState() => _TasksMainViewState();
+}
+
+class _TasksMainViewState extends State<TasksMainView> {
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TasksAppBar(),
-        Expanded(
-          child: ListView.builder(
-            itemBuilder: (context, index) => TaskCard(),
-            itemCount: 10,
-          ),
-        ),
-      ],
+    return BlocConsumer<TasksDataCubit, TasksDataState>(
+      listener: (context, state) {
+        if (state is! TasksDataStored) {
+          context.read<TasksDataCubit>().futchTasksData();
+        }
+      },
+      builder: (context, state) {
+        context.read<TasksDataCubit>().futchTasksData();
+        return Column(
+          children: [
+            TasksAppBar(),
+            Expanded(
+              child: CustomScrollView(
+                slivers: [
+                  state.tasks.isEmpty
+                      ? const SliverToBoxAdapter(
+                          child: Center(child: Text("Go add some tasks")),
+                        )
+                      : SliverList.builder(
+                          itemCount: state.tasks.length,
+                          itemBuilder: (context, index) =>
+                              TaskCard(task: state.tasks[index]),
+                        ),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
