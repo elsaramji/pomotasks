fe9893e3ba80605062e1acf472b4944252251423
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-06-28 04:07:59 +0300

    - creat task model with hive.

---
+++ lib/futures/tasks/data/task_model.dart
@@ -0,0 +1,22 @@
+import 'package:hive/hive.dart';
+
+part 'task_model.g.dart';
+
+@HiveType(typeId: 0)
+class TaskModel extends HiveObject {
+  @HiveField(0)
+  String title;
+  @HiveField(1)
+  String description;
+  @HiveField(2)
+  bool isDone;
+  @HiveField(3)
+  String id;
+
+  TaskModel({
+    required this.title,
+    required this.description,
+    this.isDone = false,
+    required this.id,
+  });
+}
