78f3e3047153e40523cd5f1ef6d34e7df0f951c6
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-06-29 04:32:54 +0300

    make widget tap able  to show full  info

--- lib/futures/tasks/presentation/widgets/task_card.dart
+++ lib/futures/tasks/presentation/widgets/task_card.dart
@@ -4,6 +4,8 @@ import 'package:pomotasks/config/themes/colors/app_colors.dart';
 import 'package:pomotasks/config/themes/styles/Buttons/texts_buttons.dart';
 import 'package:pomotasks/config/themes/styles/texts/app_texts_styles.dart';
 import 'package:pomotasks/futures/tasks/data/task_model.dart';
+import 'package:pomotasks/futures/tasks/presentation/functions/mark_task_done.dart';
+import 'package:pomotasks/futures/tasks/presentation/functions/show_Task_Description.dart';
 
 class TaskCard extends StatelessWidget {
   final TaskModel task;
@@ -12,60 +14,65 @@ class TaskCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 16.w, vertical: 8.h),
-      height: 72.h,
-      child: Row(
-        children: [
-          Container(
-            height: 48.h,
-            width: 48.w,
-            margin: EdgeInsets.only(right: 16.w),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8.r),
-              color: AppColors.boxsPrimaryColor,
+    return GestureDetector(
+      onTap: () {
+        showTaskDescription(context, task);
+      },
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 16.w, vertical: 8.h),
+        height: 72.h,
+        child: Row(
+          children: [
+            Container(
+              height: 48.h,
+              width: 48.w,
+              margin: EdgeInsets.only(right: 16.w),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.r),
+                color: AppColors.boxsPrimaryColor,
+              ),
+              child: Icon(
+                Icons.list,
+                size: 24.sp,
+                color: AppColors.textPrimaryColor,
+              ),
             ),
-            child: Icon(
-              Icons.list,
-              size: 24.sp,
-              color: AppColors.textPrimaryColor,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(task.title, style: AppTextsStyles.lexendMedium16()),
+                Text(
+                  task.description.length > 10
+                      ? '${task.description.substring(0, 10)}...'
+                      : task.description,
+                  style: AppTextsStyles.lexendRegular16(
+                    color: AppColors.primaryColor3,
+                  ),
+                ),
+              ],
             ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(task.title, style: AppTextsStyles.lexendMedium16()),
-              Text(
-                task.description.length > 50
-                    ? '${task.description.substring(0, 50)}...'
-                    : task.description,
-                style: AppTextsStyles.lexendRegular16(
-                  color: AppColors.primaryColor3,
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () {
+                // TODO
+                task.isDone = !task.isDone;
+                markTaskDone(context, task);
+              },
+              child: Text(
+                'Done',
+                style: AppTextsStyles.lexendMedium14(
+                  color: AppColors.primaryColorBackground,
                 ),
               ),
-            ],
-          ),
-          const Spacer(),
-          ElevatedButton(
-            onPressed: () {
-              // TODO
-              task.isDone = !task.isDone;
-             
-            },
-            child: Text(
-              'Done',
-              style: AppTextsStyles.lexendMedium14(
-                color: AppColors.primaryColorBackground,
+              style: AppButtonsStyle.primaryTextButtonStyle(
+                backgroundColor: AppColors.primaryColor,
+                width: 95.w,
+                hight: 32.h,
               ),
             ),
-            style: AppButtonsStyle.primaryTextButtonStyle(
-              backgroundColor: AppColors.primaryColor,
-              width: 95.w,
-              hight: 32.h,
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
