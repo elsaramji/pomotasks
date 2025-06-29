27dadd598c1d54629eda3e810bc2badd232207c9
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-06-29 04:28:13 +0300

    - add task to done data .

--- lib/futures/tasks/presentation/functions/mark_task_done.dart
+++ lib/futures/tasks/presentation/functions/mark_task_done.dart
@@ -1,1 +1,8 @@
-markTaskDone(){}
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pomotasks/futures/tasks/data/task_model.dart';
+import 'package:pomotasks/futures/tasks/presentation/cubit/tasks_data_cubit.dart';
+
+markTaskDone(BuildContext context, TaskModel task) {
+context.read<TasksDataCubit>().addTaskDone(task: task);
+}
