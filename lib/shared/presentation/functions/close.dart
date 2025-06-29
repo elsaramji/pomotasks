ee873bd936910fddd26a67fbeb2a91c3f27d57e3
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-06-29 04:36:00 +0300

    fix exit app exception

--- lib/shared/presentation/functions/close.dart
+++ lib/shared/presentation/functions/close.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:flutter/services.dart';
 
 void close(BuildContext context) {
-  context.pop();
+  SystemNavigator.pop();
 }
