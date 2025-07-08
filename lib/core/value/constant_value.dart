d2244a68c327fe9921cc8d585d5b2051b0ac8975
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-07-08 05:38:49 +0300

    add week day list

--- lib/core/value/constant_value.dart
+++ lib/core/value/constant_value.dart
@@ -1,4 +1,14 @@
 class ConstantValue {
   static const int defaultSeconds = 59;
   static const int defaultMinutes = 24;
+
+  static const List<String> weekDays = [
+    'Mon',
+    'Tue',
+    'Wed',
+    'Thu',
+    'Fri',
+    'Sat',
+    'Sun',
+  ];
 }
