1c0a17195baf708a4789dc18c0366d75980859c7
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-07-08 03:12:29 +0300

    init Hive Boxs function

---
+++ lib/core/sources/hive/init_hive.dart
@@ -0,0 +1,17 @@
+
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as PathProvider;
+import 'package:pomotasks/core/value/constant_value.dart';
+import 'package:pomotasks/futures/tasks/data/task_model.dart';
+
+Future<void> hiveinit() async {
+  final app_Path = await PathProvider.getApplicationDocumentsDirectory();
+  await Hive
+    ..init(app_Path.path)
+    ..registerAdapter(TaskModelAdapter());
+  if (!Hive.isBoxOpen(ConstantValue.tasksBoxKey) &&
+      !Hive.isBoxOpen(ConstantValue.tasksDoneBoxKey)) {
+    await Hive.openBox<TaskModel>(ConstantValue.tasksBoxKey);
+    await Hive.openBox<TaskModel>(ConstantValue.tasksDoneBoxKey);
+  }
+}
