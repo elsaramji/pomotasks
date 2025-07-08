a30f88fb5d5eaf6623aaaa9c264ccdfc197f741e
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-07-08 05:39:53 +0300

    dispaly analytics chart viwe

--- lib/futures/analytics/presentation/view/analytics_main_view.dart
+++ lib/futures/analytics/presentation/view/analytics_main_view.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pomotasks/futures/analytics/presentation/view/widgets/chart_analytics.dart';
 
 class AnalyticsMainView extends StatelessWidget {
   const AnalyticsMainView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Analytics"));
+    return Scaffold(
+      body: Container(
+        height: 400.h,
+        child: ChartAnalytics(yValues: [1, 2, 3, 4, 5, 6, 7]),
+      ),
+    );
   }
 }
